707dd0fa5de70b98aada4c3250b66ad21c5fa958
kawamoto <bahihzss@gmail.com>
2019-07-19 00:49:35 +0900

    Create the seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $products = factory(\App\Product::class, 10)->create();
+        $storages = factory(\App\Storage::class, 3)->create();
+
+        foreach ($products as $product) {
+            foreach ($storages as $storage) {
+                factory(\App\Operation::class, 100)->create([
+                    'product_id' => $product->id,
+                    'storage_id' => $storage->id,
+                ]);
+            }
+        }
     }
 }
