db9f1d8fe201c02e85bfc996c85b8f6362edea56
kawamoto <bahihzss@gmail.com>
2019-07-19 17:10:04 +0900

    Add japanese lang files.

---
+++ resources/lang/ja/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'パスワードは８文字以上で、確認欄と一致している必要があります。',
+    'reset' => 'パスワードのリセットが完了しました。',
+    'sent' => 'パスワードリセットリンクを送信しました。',
+    'token' => '不正なパスワードリセットトークンです。',
+    'user' => '該当するメールアドレスのユーザーが見つかりません。',
+];
